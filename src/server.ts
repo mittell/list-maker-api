04b70da820d01bef231911621def39ae4df1dfa2
Chris Mittell <mittell@hotmail.co.uk>
2022-04-19 18:33:27 +0900

    feat: added strict environment variables setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+import dotenv from 'dotenv';
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+	throw dotenvResult.error;
+}
+
+import config from './config';
+
 import express from 'express';
 import * as http from 'http';
 import cors from 'cors';
@@ -5,14 +14,14 @@ import helmet from 'helmet';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = config.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/', (_req: express.Request, res: express.Response) => {
 	res.status(200).send('Hello World!');
 });
 
