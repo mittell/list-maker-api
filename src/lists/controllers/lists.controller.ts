2e7dbf790dcef4a26b0a96c2bd098c264df6d869
Chris Mittell <mittell@hotmail.co.uk>
2022-04-25 20:00:29 +0900

    feat: setup basic list endpoints

---
+++ src/lists/controllers/lists.controller.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import ListService from '../services/lists.service';
+
+class ListController {
+	async getLists(_req: express.Request, res: express.Response) {
+		const lists = await ListService.list(100, 0);
+		res.status(200).send(lists);
+	}
+
+	async getListById(req: express.Request, res: express.Response) {
+		const list = await ListService.getById(req.body.id);
+		console.log(req.body.id);
+		res.status(200).send(list);
+	}
+
+	async createList(req: express.Request, res: express.Response) {
+		const listId = await ListService.create(req.body);
+		res.status(201).send({ id: listId });
+	}
+
+	async patchList(req: express.Request, res: express.Response) {
+		await ListService.patchById(req.body.id, req.body);
+		res.status(204).send();
+	}
+
+	async putList(req: express.Request, res: express.Response) {
+		await ListService.putById(req.body.id, req.body);
+		res.status(204).send();
+	}
+
+	async removeList(req: express.Request, res: express.Response) {
+		await ListService.deleteById(req.body.id);
+		res.status(204).send();
+	}
+}
+
+export default new ListController();
---
+++ src/lists/lists.routes.config.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { CommonRoutesConfig } from '../common/common.routes.config';
+import ListController from './controllers/lists.controller';
+import ListMiddleware from './middleware/lists.middleware';
+
+export class ListRoutes extends CommonRoutesConfig {
+	constructor(app: express.Application) {
+		super(app, 'ListRoutes');
+	}
+
+	configureRoutes() {
+		this.app
+			.route(`/lists`)
+			.get(ListController.getLists)
+			.post(ListController.createList);
+
+		this.app.param(`listId`, ListMiddleware.extractListId);
+		this.app
+			.route(`/lists/:listId`)
+			.get(ListController.getListById)
+			.put(ListController.putList)
+			.patch(ListController.patchList)
+			.delete(ListController.removeList);
+
+		return this.app;
+	}
+}
