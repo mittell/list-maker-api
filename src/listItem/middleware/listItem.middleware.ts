d5fd6c1210ff23a65f1eac43bae9e8c14ee2ca67
Chris Mittell <mittell@hotmail.co.uk>
2022-04-26 11:46:01 +0900

    feat: added listItem endpoint logic

---
+++ src/listItem/middleware/listItem.middleware.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+class ListItemMiddleware {
+	async extractListItemId(
+		req: express.Request,
+		_res: express.Response,
+		next: express.NextFunction
+	) {
+		req.body.id = req.params.listItemId;
+		next();
+	}
+}
+
+export default new ListItemMiddleware();
