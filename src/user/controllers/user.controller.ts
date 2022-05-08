c8c3fba73b9b64f82769e13baae546ddd4b9b290
Chris Mittell <mittell@hotmail.co.uk>
2022-05-08 17:46:52 +0900

    refactor: updated user endpoint logic

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -8,12 +8,10 @@ import UserService from '../services/user.service';
 class UserController {
 	// TODO - Exposed endpoint needs to be removed!
 	async getUsers(_req: Request, res: Response, next: NextFunction) {
-		let usersToReturn: UserToReturnDto[] = [];
-
 		await UserService.list()
 			.then((users) => {
+				let usersToReturn: UserToReturnDto[] = [];
 				users.forEach((user) => {
-					// TODO - Review implementation..
 					let userToAdd: UserToReturnDto = new UserToReturnDto();
 					userToAdd.mapFromDocument(user);
 					usersToReturn.push(userToAdd);
@@ -27,32 +25,35 @@ class UserController {
 
 	// TODO - Endpoint will need authentication, so only the user can get their own details?
 	async getUserById(req: Request, res: Response, next: NextFunction) {
-		try {
-			const existingUser = await UserService.getById(req.body.id);
+		let userId = req.body.id;
 
-			if (!existingUser) {
-				next(new NotFoundError());
-			}
+		await UserService.getById(userId)
+			.then((existingUser) => {
+				if (!existingUser) {
+					next(new NotFoundError());
+				}
 
-			let userToReturn: UserToReturnDto = new UserToReturnDto();
-			userToReturn.mapFromDocument(existingUser);
+				let userToReturn: UserToReturnDto = new UserToReturnDto();
+				userToReturn.mapFromDocument(existingUser);
 
-			res.status(200).send(userToReturn);
-		} catch (error) {
-			next(error);
-		}
+				res.status(200).send(userToReturn);
+			})
+			.catch((error) => {
+				next(error);
+			});
 	}
 
 	async createUser(req: Request, res: Response, next: NextFunction) {
 		let userToCreate: UserToCreateDto = new UserToCreateDto();
+
 		userToCreate.mapFromRequest(req.body);
 
 		// TODO - Password generation and hashing needed to happen here!!!
 
 		await UserService.create(req.body)
 			.then((id) => {
-				userToCreate.updateId(id);
-				res.status(201).send({ id: userToCreate.getId() });
+				userToCreate.id = id;
+				res.status(201).send({ id: userToCreate.id });
 			})
 			.catch((error) => {
 				next(error);
@@ -62,6 +63,7 @@ class UserController {
 	// TODO - Endpoint will need authentication, so only the user can get update own details?
 	async patchUser(req: Request, res: Response, next: NextFunction) {
 		let userToUpdate: UserToUpdateDto = new UserToUpdateDto();
+
 		userToUpdate.mapFromRequest(req.body);
 
 		// TODO - Password hashing needed to happen here!!!
@@ -78,6 +80,7 @@ class UserController {
 	// TODO - Endpoint will need authentication, so only the user can get update own details?
 	async putUser(req: Request, res: Response, next: NextFunction) {
 		let userToUpdate: UserToUpdateDto = new UserToUpdateDto();
+
 		userToUpdate.mapFromRequest(req.body);
 
 		// TODO - Password hashing needed to happen here!!!
@@ -93,16 +96,18 @@ class UserController {
 
 	// TODO - Exposed endpoint needs to be removed???
 	async removeUser(req: Request, res: Response, next: NextFunction) {
-		const existingUser = await UserService.getById(req.body.id);
+		let userId = req.body.id;
 
-		if (!existingUser) {
-			next(new NotFoundError());
-		}
+		await UserService.getById(userId)
+			.then(async (existingUser) => {
+				if (!existingUser) {
+					next(new NotFoundError());
+				}
 
-		//@ts-expect-error
-		await UserService.deleteById(existingUser._id)
-			.then(() => {
-				res.status(204).send();
+				//@ts-expect-error
+				await UserService.deleteById(existingUser._id).then(() => {
+					res.status(204).send();
+				});
 			})
 			.catch((error) => {
 				next(error);
--- src/user/dto/userToReturn.dto.ts
+++ src/user/dto/userToReturn.dto.ts
@@ -1,12 +1,32 @@
-// TODO - Review implementation... consider failed mapping?
+import { MappingError } from '../../common/types/error.type';
+
 export class UserToReturnDto {
 	id!: string;
 	username!: string;
 	email!: string;
 
 	mapFromDocument(list: any) {
-		this.id = list._doc._id;
-		this.username = list.username;
-		this.email = list.email;
+		let id = list._doc._id;
+		let username = list.username;
+		let email = list.email;
+
+		if (
+			id === null ||
+			undefined ||
+			'' ||
+			username === null ||
+			undefined ||
+			'' ||
+			email === null ||
+			undefined ||
+			''
+		) {
+			console.log('Unable to map User from Document');
+			throw new MappingError('Unable to map User from Document');
+		}
+
+		this.id = id;
+		this.username = username;
+		this.email = email;
 	}
 }
